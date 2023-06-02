5d1d7697dd028c3dd945c5375473a9a87b81dce4
jimenezroa901 <130469757+jimenezroa901@users.noreply.github.com>
2023-06-02 16:46:24 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,65 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-            int opcion = 0;
-            decimal ingresos = 0;
-            decimal gastos = 0;
+int np = 0, p2 = 0, p1 = 0, nu = 0;
 
-            do
-            {
-                Console.WriteLine("Seleccione una opción:");
-                Console.WriteLine("1. Registrar ingreso");
-                Console.WriteLine("2. Registrar gasto");
-                Console.WriteLine("3. Mostrar balance");
-                Console.WriteLine("4. Salir");
+double total = 0, tv = 0, resultado = 0;
 
-                try
-                {
-                    opcion = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Opción inválida");
-                    opcion = 0;
-                }
 
-                switch (opcion)
-                {
-                    case 1:
-                        Console.WriteLine("Ingrese el monto del ingreso:");
-                        try
-                        {
-                            decimal montoIngreso = Convert.ToDecimal(Console.ReadLine());
-                            ingresos += montoIngreso;
-                            Console.WriteLine("Ingreso registrado");
-                        }
-                        catch (FormatException)
-                        {
-                            Console.WriteLine("Monto inválido");
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Ingrese el monto del gasto:");
-                        try
-                        {
-                            decimal montoGasto = Convert.ToDecimal(Console.ReadLine());
-                            gastos += montoGasto;
-                            Console.WriteLine("Gasto registrado");
-                        }
-                        catch (FormatException)
-                        {
-                            Console.WriteLine("Monto inválido");
-                        }
-                        break;
-                    case 3:
-                        decimal balance = ingresos - gastos;
-                        Console.WriteLine($"Ingresos: {ingresos}");
-                        Console.WriteLine($"Gastos: {gastos}");
-                        Console.WriteLine($"Balance: {balance}");
-                        break;
-                    case 4:
-                        Console.WriteLine("Saliendo...");
-                        break;
-                    default:
-                        Console.WriteLine("Opción inválida");
-                        break;
-                }
-            } while (opcion != 4);
+
+Console.WriteLine("numero de usuarios");
+nu = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < nu; i++)
+{
+    Console.WriteLine("digite el valor del producto porfavor");
+    p1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("digite el valor del producto porfavor");
+    p2 = int.Parse(Console.ReadLine());
+    resultado = p1 + p2;
+
+    if (resultado <= 100.000)
+    {
+        total = resultado * 0.05;
+        Console.WriteLine(" el total a pagar es" + total);
+        tv++;
+    }
+
+
+    else if (resultado > 100000 || resultado <= 400000)
+    {
+        total = resultado * 0.07;
+        Console.WriteLine("el total a pagar es de " + total);
+        tv++;
+    }
+
+
+    if (resultado > 400000 || resultado <= 900000)
+    {
+
+        total = resultado * 0.12;
+        Console.WriteLine("el total a pagar es de " + total);
+        tv++;
+    }
+
+
+    else
+    {
+        total = resultado * 0.15;
+        Console.WriteLine("el total a pagar es de " + total);
+        tv++;
+
+    }
+}
+Console.WriteLine("el total de ventas es de " + tv);
